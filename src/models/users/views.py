a1432de3c4c2d44ec230c2e160078b181a525c67
asimonia <alex.simonian@gmail.com>
2017-03-10 00:11:29 -0600

    Add users views

---
+++ src/models/users/views.py
@@ -0,0 +1,23 @@
+from flask import blueprints
+
+user_blueprint = blueprints.Blueprint('users', __name__)
+
+
+@user_blueprint.route('/login')
+def login_user():
+	pass
+
+
+@user_blueprint.route('/register')
+def register_user():
+	pass
+
+
+@user_blueprint.route('/alerts')
+def user_alerts():
+	pass
+
+
+@user_blueprint.route('/logout')
+def logout_user():
+	pass
