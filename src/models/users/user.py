4014f08cbdc7382baa96dec91b971d2e8b4db403
asimonia <alex.simonian@gmail.com>
2017-03-10 10:56:03 -0600

    Add method to check username and password

--- src/models/users/user.py
+++ src/models/users/user.py
@@ -1,8 +1,31 @@
+from src.common.database import Database
+import uuid
+
 class User:
 
-	def __init__(self, email, password):
+	def __init__(self, email, password, _id=None):
+		"""Create a user with an email, password, unique id"""
 		self.email = email
 		self.password = password
+		self._id = uuid.uuid4().hex if _id is None else _id
 
 	def __repr__(self):
 		return "<User {}>".format(self.email)
+
+	@staticmethod
+	def is_login_valid(email, password):
+		"""
+		Verifies that an email/password combo is valid
+		Checks that the email exists, and the password
+		associated to that email is correct.
+		Uses sha512 hashed password.
+		"""
+		user_data = Database.find_one("users", {'email': email})
+		if user_data is None:
+			# Tell user the email doesn't exist
+			pass
+		if not Utils.check_hashed_password(password, user_data['password']):
+			# Tell user that their password is wrong
+			pass
+
+		return True
