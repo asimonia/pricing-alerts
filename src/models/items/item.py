262e322bbba2262fe2477ecd635990c93517c691
asimonia <alex.simonian@gmail.com>
2017-03-11 13:42:29 -0600

    Added requests lib and modified item class

--- src/models/items/item.py
+++ src/models/items/item.py
@@ -7,3 +7,7 @@ class Item:
 
 	def __repr__(self):
 		return "<Item {} with URL {}>".format(self.name, self.url)
+
+	def load_item(self):
+		# Amazon HTML
+		# <span id="priceblock_ourprice" class="a-size-medium a-color-price">$24.42</span>
--- src/models/items/views.py
+++ src/models/items/views.py
@@ -7,10 +7,3 @@ item_blueprint = blueprints.Blueprint('items', __name__)
 def item_page(name):
 	pass
 
-@item_blueprint.route('/load')
-def load_item():
-	"""
-	Loads an item's data using their store and return a JSON representation of it.
-	:return:
-	"""
-	pass
