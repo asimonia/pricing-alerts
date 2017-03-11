524d42f1176b4d09107e0f7a44237ce24a97e3c0
asimonia <alex.simonian@gmail.com>
2017-03-10 18:44:52 -0600

    Modify try/catch

--- src/models/users/views.py
+++ src/models/users/views.py
@@ -1,6 +1,7 @@
 from flask import blueprints, request, session, url_for, render_template
 from werkzeug.utils import redirect
 from src.models.users.user import User
+from src.models.users.user import UserErrors
 
 user_blueprint = blueprints.Blueprint('users', __name__)
 
@@ -16,8 +17,8 @@ def login_user():
 			if User.is_login_valid(email, password):
 				session['email'] = email
 				return redirect(url_for(".user_alerts"))
-		except:
-			return "Your password is wrong."
+		except UserErrors.UserError as e:
+			return e.message
 	
 	return render_template("users/login.html")
 
