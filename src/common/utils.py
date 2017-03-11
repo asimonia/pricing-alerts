8ae4594d4f4157568db0dc5cad4d07b8f1142218
asimonia <alex.simonian@gmail.com>
2017-03-10 21:06:30 -0600

    Add static method for email address

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,6 +1,12 @@
+import re
 from passlib.hash import pbkdf2_sha512
 
 class Utils:
+
+	@staticmethod
+	def email_is_valid(email):
+		email_address_matcher = re.compile('^[\w-]+@([\w-]+\.)+[\w]+$')
+		return True if email_address_matcher.match(email) else False
 
 	@staticmethod
 	def hash_password(password):
