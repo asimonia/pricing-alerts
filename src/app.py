d738670bafa45e36c793d4ec6d7bdc45fa1218bb
asimonia <alex.simonian@gmail.com>
2017-03-12 14:27:18 -0500

    Add index.html and dependencies

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from common.database import Database
 
 
@@ -10,6 +10,11 @@ app.secret_key = "123"
 @app.before_first_request
 def init_db():
 	Database.initialize()
+
+
+@app.route('/')
+def home():
+	return render_template('home.html')
 
 
 from models.users.views import user_blueprint
