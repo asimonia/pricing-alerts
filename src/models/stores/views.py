be605a290a87dbb42c60239c1e93741d6a5f33e7
asimonia <alex.simonian@gmail.com>
2017-03-12 16:06:49 -0500

    Added alerts and stores

--- src/models/stores/views.py
+++ src/models/stores/views.py
@@ -3,6 +3,11 @@ from flask import blueprints
 store_blueprint = blueprints.Blueprint('stores', __name__)
 
 
+@store_blueprint.route('/')
+def index():
+	return "This is the stores index"
+
+
 @store_blueprint.route('/store/<string:name>')
 def store_page():
 	pass
